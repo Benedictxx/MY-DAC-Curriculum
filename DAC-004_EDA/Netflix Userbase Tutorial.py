66e507cfdae1b718abafc85955a96d1cb2bbd6fa
Benedict <xlong008@mymail.sim.edu.sg>
2024-10-22 19:50:50 +0800

    final-changes

---
+++ DAC-004_EDA/Netflix Userbase Tutorial.py
@@ -0,0 +1,9 @@
+#Netflix Userbase Tutorial 
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+#df = pd.read_csv("C:\Users\lxx55\Downloads\Netflix_Userbase.csv")
+
